151a8d9e96296380887aa01713398dce72970bc2
Tan Nguyen <tannv.dts@gmail.com>
2015-11-10 19:57:09 +0700

    push database script

---
+++ database/project_referal1.sql
@@ -0,0 +1,69 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.6.26-log : Database - referal_1
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`referal_1` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `referal_1`;
+
+/*Table structure for table `image` */
+
+DROP TABLE IF EXISTS `image`;
+
+CREATE TABLE `image` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `UID` varchar(255) DEFAULT NULL,
+  `FileName` varchar(255) DEFAULT NULL,
+  `FileLocation` varchar(255) DEFAULT NULL,
+  `Description` text,
+  `Enable` varchar(1) DEFAULT 'Y',
+  `CreatedBy` int(11) DEFAULT NULL,
+  `CreatedDate` timestamp NULL DEFAULT NULL,
+  `ModifiedBy` int(11) DEFAULT NULL,
+  `ModifiedDate` timestamp NULL DEFAULT NULL,
+  `UserAccountID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_IMAGE_USERACCOUNT` (`UserAccountID`),
+  CONSTRAINT `FK_IMAGE_USERACCOUNT` FOREIGN KEY (`UserAccountID`) REFERENCES `useraccount` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `useraccount` */
+
+DROP TABLE IF EXISTS `useraccount`;
+
+CREATE TABLE `useraccount` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `UID` varchar(255) DEFAULT NULL,
+  `UserName` varchar(50) DEFAULT NULL,
+  `Email` varchar(100) DEFAULT NULL,
+  `Password` varchar(255) DEFAULT NULL,
+  `UserType` varchar(10) DEFAULT NULL,
+  `FirstName` varchar(50) DEFAULT NULL,
+  `LastName` varchar(50) DEFAULT NULL,
+  `Gender` varchar(1) DEFAULT NULL,
+  `DOB` date DEFAULT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  `PhoneNumber` varchar(20) DEFAULT NULL,
+  `Enable` varchar(1) DEFAULT NULL,
+  `CreatedBy` int(11) DEFAULT NULL,
+  `CreatedDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `ModifiedBy` int(11) DEFAULT NULL,
+  `ModifiedDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `UserName` (`UserName`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
